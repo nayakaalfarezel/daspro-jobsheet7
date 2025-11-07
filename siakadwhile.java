b99281966e0bd4c19cc984479bb56867434ebcd9
nayakaalfarezel <nayakaalfarezel422@gmail.com>
2025-11-07 14:39:47 +0700

    siakadwhile

--- siakadwhile.java
+++ siakadwhile.java
@@ -3,27 +3,25 @@ public class siakadwhile {
     public static void main(String[] args) {
                Scanner sc = new Scanner(System.in);
         int nilai, jml, i = 0;
+        i=0;
 
         System.out.print("Masukkan jumlah mahasiswa: "); {
-            System.out.print("Input tidak valid. Masukkan jumlah mahasiswa: ");
-           jml = sc.nextInt();
-        }
-        
-
+            jml = sc.nextInt();
         while (i < jml) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": "); {
-                System.out.print("Input tidak valid. Masukkan nilai (0-100): ");
+                System.out.print("Input tidak valid. ");
                 nilai =sc.nextInt();
             }
             
 
             if (nilai < 0 || nilai > 100) {
-                System.out.println("Nilai tidak valid, masukkan nilai yang valid (0-100).");
+                System.out.println("Nilai tidak valid, masukkan nilai yang valid.");
                 continue; 
             }
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("pertahankan");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -43,5 +41,5 @@ public class siakadwhile {
 
         sc.close(); 
     }
-    
-}
+}}
+
