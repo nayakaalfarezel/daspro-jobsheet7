a0bcaa1737a646ba0cc4e969f51de03c59768d96
nayakaalfarezel <nayakaalfarezel422@gmail.com>
2025-11-07 10:25:54 +0700

    siakadwhile

---
+++ siakadwhile.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class siakadwhile {
+    public static void main(String[] args) {
+               Scanner sc = new Scanner(System.in);
+        int nilai, jml, i = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa: "); {
+            System.out.print("Input tidak valid. Masukkan jumlah mahasiswa: ");
+           jml = sc.nextInt();
+        }
+        
+
+        while (i < jml) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": "); {
+                System.out.print("Input tidak valid. Masukkan nilai (0-100): ");
+                nilai =sc.nextInt();
+            }
+            
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid, masukkan nilai yang valid (0-100).");
+                continue; 
+            }
+
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
+
+            i++;
+        }
+
+        sc.close(); 
+    }
+    
+}
